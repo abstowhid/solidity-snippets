753b3bf6d1eb19e9d4b8224821d90dcdd3b82d77
Towhid <blatowhid@gmail.com>
2025-03-06 21:39:11 +0600

    Update hash-return.sol

--- hash-return.sol
+++ hash-return.sol
@@ -10,6 +10,7 @@ address to;
 }
 mapping (bytes32=>Transaction) transactions;
 mapping (address=>bytes32[]) allTransactionsPerOwner;
+event Tran(bytes32 _hash);
 function makeTransaction(string memory _senderName,string memory _receiverName,string memory _tokenName, uint amount,address to) public returns (bytes32){
     Transaction memory newT=Transaction({
         senderName:_senderName,
@@ -21,8 +22,9 @@ function makeTransaction(string memory _senderName,string memory _receiverName,s
     bytes32 hash = keccak256(abi.encodePacked(_senderName,_receiverName,_tokenName,amount,to,msg.sender,block.timestamp,block.number));
     transactions[hash]=newT;
     allTransactionsPerOwner[msg.sender].push(hash);
+    emit Tran(hash);
     return hash;
 }
 //In Solidity, state-changing functions do not return values when called via a transaction (because transactions do not return data; they execute changes in the blockchain).
-
+//to solve this we used event 
 }
